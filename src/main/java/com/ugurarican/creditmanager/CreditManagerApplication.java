7d4d7559397c708c3e2fa0abfbf60762e5d1a837
Ugur Arican <ugur-arican@hotmail.com>
2023-02-24 15:11:12 +0300

    first Commit

---
+++ src/main/java/com/ugurarican/creditmanager/CreditManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ugurarican.creditmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ugurarican/creditmanager/CreditManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugurarican.creditmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
